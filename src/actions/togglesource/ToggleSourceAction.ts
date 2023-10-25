912c230a45a42cda6f6b311c87c251555a474db2
theca11 <48060851+theca11@users.noreply.github.com>
2023-10-25 10:53:15 +0200

    fix: refetch sources state on collection change
    No event is emitted by OBS related to sources, so a new
    refetch is needed

--- src/actions/togglesource/ToggleSourceAction.ts
+++ src/actions/togglesource/ToggleSourceAction.ts
@@ -22,6 +22,21 @@ export class ToggleSourceAction extends AbstractStatefulRequestAction<ActionSett
 				$SD.sendToPropertyInspector(context, piPayload, action);
 			}
 		});
+
+		// Refetch states on scene collection changed, since source-specific events are not emitted
+		sockets.forEach((socket, socketIdx) => {
+			socket.on('CurrentSceneCollectionChanged', async () => {
+				console.log('CurrentSceneCollectionChanged');
+				for (const [context, { settings, states }] of this._contexts) {
+					if (!settings[socketIdx]) return;
+					const newState = await this.fetchState(settings[socketIdx]!, socketIdx).catch(() => StateEnum.Unavailable);	// to-do: review consistency for returning Unavailable - should I return this every time config is not vali, or better off?
+					if (newState !== states[socketIdx]) {
+						this._updateSocketState(context, socketIdx, newState);
+						this.updateKeyImage(context);
+					}
+				}
+			});
+		});
 	}
 
 	getPayloadFromSettings(socketIdx: number, settings: Record<string, never> | Partial<ActionSettings>, state: StateEnum, desiredState?: number | undefined): BatchRequestPayload {
