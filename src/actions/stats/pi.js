2e0aa441875004fc5da91a38749c2ea26a7cf08b
theca11 <48060851+theca11@users.noreply.github.com>
2024-01-14 11:52:57 +0100

    fix: newly added stat buttons not working

--- src/actions/stats/pi.js
+++ src/actions/stats/pi.js
@@ -9,36 +9,43 @@ document.querySelectorAll('button.add').forEach((button) =>
 			statField,
 		);
 		formEl.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
+
+		formEl.querySelectorAll('.stat-items .icon-button').forEach((el) =>
+			el.addEventListener('click', onClickHandler),
+		);
 	}),
 );
 
 document.querySelectorAll('.stat-items .icon-button').forEach((el) =>
-	el.addEventListener('click', (event) => {
-		event.preventDefault();
-		if (!event.target.matches('button')) return;
-		const formEl = event.target.closest('form');
-		if (event.target.matches('.down')) {
-			const group = event.target.closest('.stat-group');
-			const parent = group.parentNode;
-			const next = group.nextElementSibling;
-			if (next) {
-				parent.insertBefore(group, next.nextElementSibling);
-			}
-		}
-		else if (event.target.matches('.up')) {
-			const group = event.target.closest('.stat-group');
-			const parent = group.parentNode;
-			const previous = group.previousElementSibling;
-			if (previous) {
-				parent.insertBefore(group, previous);
-			}
-		}
-		else if (event.target.matches('.remove')) {
-			event.target.closest('.stat-group')?.remove();
-		}
-		formEl.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
-	}),
+	el.addEventListener('click', onClickHandler),
 );
+
+function onClickHandler(event) {
+	event.preventDefault();
+	if (!event.target.matches('button')) return;
+	const formEl = event.target.closest('form');
+	if (event.target.matches('.down')) {
+		const group = event.target.closest('.stat-group');
+		const parent = group.parentNode;
+		const next = group.nextElementSibling;
+		if (next) {
+			parent.insertBefore(group, next.nextElementSibling);
+		}
+	}
+	else if (event.target.matches('.up')) {
+		const group = event.target.closest('.stat-group');
+		const parent = group.parentNode;
+		const previous = group.previousElementSibling;
+		if (previous) {
+			parent.insertBefore(group, previous);
+		}
+	}
+	else if (event.target.matches('.remove')) {
+		event.target.closest('.stat-group')?.remove();
+	}
+	formEl.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
+}
+
 
 // Hide long press and custom image advanced options
 document.querySelector('#longPress').style.display = 'none';
